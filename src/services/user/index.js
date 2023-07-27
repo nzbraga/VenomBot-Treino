6510308f2c797eab7f0f784576780986c8085758
nabraga <nzbg@hotmail.com>
2023-07-27 11:36:02 -0300

    first commit

---
+++ src/services/user/index.js
@@ -0,0 +1,67 @@
+const user = {
+
+    msg:
+    `Olá , sou a atendente virtual de 
+    Monique Braga Nail Designer 🎀 \n
+    Como posso ajudar? \n\n
+    1 - Agenda \n
+    2 - Instagram \n
+    3 - Serviços \n
+    4 - Cuidados \n\n  `
+    ,
+    
+    agenda:
+    `Terça:\n
+    - 09:00\n
+    - 13:00\n
+    Quarta:\n
+    - 09:00\n
+    - 13:00\n
+    Quinta:\n
+    - 09:00\n
+    - 13:00\n
+    Sexta:\n
+    - 09:00\n
+    - 13:00\n
+    Sab:\n
+    - 09:00\n
+    - 13:00\n
+    
+    escolha um horario e aguarde a confirmação!\n
+    *atualizado 27/07/2023`
+    ,
+
+    media:
+    `Acesse para conferir alguns dos meus trabalhos:\n https://www.instagram.com/moniquebragaoficial`
+    ,
+
+    services:
+    `Serviços: \n\n
+    - Banho de Gel \n
+    - Blindagem \n
+    - Fibra \n
+    - Gel Tips \n
+    - Decorações !          
+    `,
+
+    care:
+    `Alguns cuidados que devemos ter: \n
+                          
+    - Evite morder as unhas \n
+    - Hidrate mãos e cutículas. \n
+    - Manutenção regular. \n
+    - Evite remoção por conta própria. \n
+    - Permita intervalos de descanso. \n
+    - Observe sinais de problemas. \n
+    `,
+
+    pause:
+    'Vou dar uma pausa, volto em 1 minuto!'
+    ,
+
+    iBack:
+    'Voltei'
+    ,
+}
+
+module.exports = user
